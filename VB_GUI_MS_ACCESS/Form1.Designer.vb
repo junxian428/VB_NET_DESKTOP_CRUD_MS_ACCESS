ea6e4b643e01051cee7aaa9a0edfd457a77ed389
junxian428 <holeyiho@gmail.com>
2021-12-24 23:25:34 +0800

    This is VB.NET CRUD MS ACCESS

---
+++ VB_GUI_MS_ACCESS/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Staff_ID_Box = New System.Windows.Forms.TextBox()
+        Me.Staff_Name_Box = New System.Windows.Forms.TextBox()
+        Me.Staff_Age_Box = New System.Windows.Forms.TextBox()
+        Me.Staff_Salary_Box = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Staff_ID_Box
+        '
+        Me.Staff_ID_Box.Location = New System.Drawing.Point(303, 85)
+        Me.Staff_ID_Box.Name = "Staff_ID_Box"
+        Me.Staff_ID_Box.Size = New System.Drawing.Size(100, 20)
+        Me.Staff_ID_Box.TabIndex = 0
+        '
+        'Staff_Name_Box
+        '
+        Me.Staff_Name_Box.Location = New System.Drawing.Point(303, 111)
+        Me.Staff_Name_Box.Name = "Staff_Name_Box"
+        Me.Staff_Name_Box.Size = New System.Drawing.Size(100, 20)
+        Me.Staff_Name_Box.TabIndex = 1
+        '
+        'Staff_Age_Box
+        '
+        Me.Staff_Age_Box.Location = New System.Drawing.Point(303, 137)
+        Me.Staff_Age_Box.Name = "Staff_Age_Box"
+        Me.Staff_Age_Box.Size = New System.Drawing.Size(100, 20)
+        Me.Staff_Age_Box.TabIndex = 2
+        '
+        'Staff_Salary_Box
+        '
+        Me.Staff_Salary_Box.Location = New System.Drawing.Point(303, 164)
+        Me.Staff_Salary_Box.Name = "Staff_Salary_Box"
+        Me.Staff_Salary_Box.Size = New System.Drawing.Size(100, 20)
+        Me.Staff_Salary_Box.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(516, 137)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(237, 88)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Staff ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(237, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Staff Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(236, 140)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Staff Age"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(236, 167)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(61, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Staff Salary"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(597, 137)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Update"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(516, 167)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Delete"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(516, 37)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Staff_Salary_Box)
+        Me.Controls.Add(Me.Staff_Age_Box)
+        Me.Controls.Add(Me.Staff_Name_Box)
+        Me.Controls.Add(Me.Staff_ID_Box)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Staff_ID_Box As TextBox
+    Friend WithEvents Staff_Name_Box As TextBox
+    Friend WithEvents Staff_Age_Box As TextBox
+    Friend WithEvents Staff_Salary_Box As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+End Class
---
+++ VB_GUI_MS_ACCESS/Form1.vb
@@ -0,0 +1,196 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+
+    Dim dbProvider As String
+    Dim dbSource As String
+    Dim con As New OleDb.OleDbConnection
+    Dim sql As String
+    Dim da As OleDb.OleDbDataAdapter
+    Dim Reader As OleDb.OleDbDataReader
+    Dim Command As OleDb.OleDbCommand
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        dbProvider = "PROVIDER=Microsoft.Jet.OLEDB.4.0;" ' MDB
+        'dbProvider = "PROVIDER=Microsoft.Ace.OLEDB.12.0;" 'ACCDB
+        dbSource = "Data Source=C:\Users\USer\source\repos\D210044A_Ho_Weng_Yin\D210044A_Ho_Weng_Yin\DatabaseForVB.mdb;  Persist Security Info= False;"
+        con.ConnectionString = dbProvider & dbSource
+        'Staff_ID_Box Staff_Name_Box Staff_Age_Box Staff_Salary_Box
+
+        Try
+            MessageBox.Show("Database is Opened")
+            Using myconnection As New OleDbConnection(con.ConnectionString)
+                myconnection.Open()
+                'Dim sqlQry As String = "INSERT INTO [tbl_user] ([username], [password]) VALUES (@usernme, @passwrd)"
+                con.Open()
+                sql = "INSERT INTO Staff_Table (Staff_Name, Staff_Age,Staff_Salary) VALUES ('" + Staff_Name_Box.Text + "','" + Staff_Age_Box.Text + "','" + Staff_Salary_Box.Text + "');"
+                da = New OleDb.OleDbDataAdapter(sql, con)
+                Using cmd As New OleDbCommand(sql, myconnection)
+                    cmd.ExecuteNonQuery()
+                End Using
+                myconnection.Close()
+            End Using
+
+
+
+        Catch ex As Exception
+            ' Show the exception's message.
+            MessageBox.Show(ex.Message)
+            ' Show the stack trace, which is a list of methods
+            ' that are currently executing.
+            MessageBox.Show("Stack Trace: " & vbCrLf & ex.StackTrace)
+        Finally
+            ' This line executes whether or not the exception occurs.
+            MessageBox.Show("Operation is Done")
+        End Try
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        dbProvider = "PROVIDER=Microsoft.Jet.OLEDB.4.0;" ' MDB
+        'dbProvider = "PROVIDER=Microsoft.Ace.OLEDB.12.0;" 'ACCDB
+        dbSource = "Data Source=C:\Users\USer\source\repos\D210044A_Ho_Weng_Yin\D210044A_Ho_Weng_Yin\DatabaseForVB.mdb;  Persist Security Info= False;"
+        con.ConnectionString = dbProvider & dbSource
+        'Staff_ID_Box Staff_Name_Box Staff_Age_Box Staff_Salary_Box
+
+        Try
+            MessageBox.Show("Database is Opened")
+            Using myconnection As New OleDbConnection(con.ConnectionString)
+                myconnection.Open()
+                'Dim sqlQry As String = "INSERT INTO [tbl_user] ([username], [password]) VALUES (@usernme, @passwrd)"
+                con.Open()
+                sql = "Update Staff_Table set Staff_Name='" + Staff_Name_Box.Text + "',Staff_Age='" + Staff_Age_Box.Text + "',Staff_Salary='" + Staff_Salary_Box.Text + "' WHERE ID=" + Staff_ID_Box.Text + ";"
+                da = New OleDb.OleDbDataAdapter(sql, con)
+                Using cmd As New OleDbCommand(sql, myconnection)
+                    cmd.ExecuteNonQuery()
+                End Using
+                myconnection.Close()
+            End Using
+
+
+
+        Catch ex As Exception
+            ' Show the exception's message.
+            MessageBox.Show(ex.Message)
+            ' Show the stack trace, which is a list of methods
+            ' that are currently executing.
+            MessageBox.Show("Stack Trace: " & vbCrLf & ex.StackTrace)
+        Finally
+            ' This line executes whether or not the exception occurs.
+            MessageBox.Show("Operation is Done")
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        dbProvider = "PROVIDER=Microsoft.Jet.OLEDB.4.0;" ' MDB
+        'dbProvider = "PROVIDER=Microsoft.Ace.OLEDB.12.0;" 'ACCDB
+        dbSource = "Data Source=C:\Users\USer\source\repos\D210044A_Ho_Weng_Yin\D210044A_Ho_Weng_Yin\DatabaseForVB.mdb;  Persist Security Info= False;"
+        con.ConnectionString = dbProvider & dbSource
+        'Staff_ID_Box Staff_Name_Box Staff_Age_Box Staff_Salary_Box
+
+        Try
+            MessageBox.Show("Database is Opened")
+            Using myconnection As New OleDbConnection(con.ConnectionString)
+                myconnection.Open()
+                'Dim sqlQry As String = "INSERT INTO [tbl_user] ([username], [password]) VALUES (@usernme, @passwrd)"
+                con.Open()
+                sql = "Delete from Staff_Table " + " WHERE ID=" + Staff_ID_Box.Text + ";"
+                da = New OleDb.OleDbDataAdapter(sql, con)
+                Using cmd As New OleDbCommand(sql, myconnection)
+                    cmd.ExecuteNonQuery()
+                End Using
+                myconnection.Close()
+            End Using
+
+
+
+        Catch ex As Exception
+            ' Show the exception's message.
+            MessageBox.Show(ex.Message)
+            ' Show the stack trace, which is a list of methods
+            ' that are currently executing.
+            MessageBox.Show("Stack Trace: " & vbCrLf & ex.StackTrace)
+        Finally
+            ' This line executes whether or not the exception occurs.
+            MessageBox.Show("Operation is Done")
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dbProvider = "PROVIDER=Microsoft.Jet.OLEDB.4.0;" ' MDB
+        'dbProvider = "PROVIDER=Microsoft.Ace.OLEDB.12.0;" 'ACCDB
+        dbSource = "Data Source=C:\Users\USer\source\repos\D210044A_Ho_Weng_Yin\D210044A_Ho_Weng_Yin\DatabaseForVB.mdb;  Persist Security Info= False;"
+        con.ConnectionString = dbProvider & dbSource
+        'Staff_ID_Box Staff_Name_Box Staff_Age_Box Staff_Salary_Box
+        Dim cmdString As String = "Select * from Staff_Table"
+        Try
+
+            Using myconnection As New OleDbConnection(con.ConnectionString)
+                myconnection.Open()
+                Dim TheCommand As OleDbCommand = New OleDbCommand(cmdString, myconnection)
+                TheCommand.CommandType = CommandType.Text
+                Dim TheDataReader As OleDbDataReader = TheCommand.ExecuteReader()
+                While TheDataReader.Read()
+                    Console.WriteLine(TheDataReader("Staff_Name").ToString())
+                    ComboBox1.Items.Add(TheDataReader("Staff_Name").ToString())
+                End While
+                myconnection.Close()
+            End Using
+
+
+
+        Catch ex As Exception
+            ' Show the exception's message.
+            MessageBox.Show(ex.Message)
+            ' Show the stack trace, which is a list of methods
+            ' that are currently executing.
+            MessageBox.Show("Stack Trace: " & vbCrLf & ex.StackTrace)
+        Finally
+            ' This line executes whether or not the exception occurs.
+            MessageBox.Show("Operation is Done")
+        End Try
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        dbProvider = "PROVIDER=Microsoft.Jet.OLEDB.4.0;" ' MDB
+        'dbProvider = "PROVIDER=Microsoft.Ace.OLEDB.12.0;" 'ACCDB
+        dbSource = "Data Source=C:\Users\USer\source\repos\D210044A_Ho_Weng_Yin\D210044A_Ho_Weng_Yin\DatabaseForVB.mdb;  Persist Security Info= False;"
+        con.ConnectionString = dbProvider & dbSource
+        'Staff_ID_Box Staff_Name_Box Staff_Age_Box Staff_Salary_Box
+        Dim cmdString As String = "Select * from Staff_Table '" + ComboBox1.Text + "';"
+        Try
+
+            Using myconnection As New OleDbConnection(con.ConnectionString)
+                myconnection.Open()
+                Dim TheCommand As OleDbCommand = New OleDbCommand(cmdString, myconnection)
+                TheCommand.CommandType = CommandType.Text
+                Dim TheDataReader As OleDbDataReader = TheCommand.ExecuteReader()
+                While TheDataReader.Read()
+                    Console.WriteLine(TheDataReader("Staff_Name").ToString())
+                    'Staff_ID_Box Staff_Name_Box Staff_Age_Box Staff_Salary_Box
+                    Staff_ID_Box.Text = TheDataReader("ID").ToString()
+                    Staff_Name_Box.Text = TheDataReader("Staff_Name").ToString()
+                    Staff_Age_Box.Text = TheDataReader("Staff_Age").ToString()
+                    Staff_Salary_Box.Text = TheDataReader("Staff_Salary").ToString()
+
+
+                End While
+                myconnection.Close()
+            End Using
+
+
+
+        Catch ex As Exception
+            ' Show the exception's message.
+            MessageBox.Show(ex.Message)
+            ' Show the stack trace, which is a list of methods
+            ' that are currently executing.
+            MessageBox.Show("Stack Trace: " & vbCrLf & ex.StackTrace)
+        Finally
+            ' This line executes whether or not the exception occurs.
+            MessageBox.Show("Operation is Done")
+        End Try
+    End Sub
+End Class
